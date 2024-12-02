fc384f8d452f4d20a562582f38ef361cc60ad4c9
Ouvill <contact@ouvill.net>
2024-12-02 19:25:56 +0900

    add AWS integration and type definitions for Hono app

--- projects/cloudflare_worker_aws_lambda_samplel/worker/src/index.ts
+++ projects/cloudflare_worker_aws_lambda_samplel/worker/src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
+import { AwsClient } from "aws4fetch"
+import { HonoType } from './type'
 
-const app = new Hono()
+const app = new Hono<HonoType>()
+  .get('/', (c) => {
+    // const client = newAws4Client(c)
+    const client = new AwsClient({
+      accessKeyId: c.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: c.env.AWS_SECRET_ACCESS_KEY,
+    })
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+    return client.fetch(c.env.AWS_Lambda_Function_URL)
+  });
 
 export default app
